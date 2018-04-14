8e4074f39c1e641a0cf0f3034b8c2d163a3042e7
Samarth Gowda <samarth.m.gowda@gmail.com>
2018-04-14 11:25:50 +0530

    Add files via upload

--- basic.py
+++ basic.py
@@ -5,9 +5,13 @@ import tkinter as tk
 from tkinter import ttk
 import matplotlib.animation as animation
 from matplotlib import style
-
+import matplotlib.pyplot as plt
 from tkinter import *
-
+import threading
+import time
+import random
+import numpy as np
+import collections
 
 matplotlib.use("TkAgg")
 readings = []
@@ -23,51 +27,163 @@ gestures={}
 gestures['fist'] = [1,2,3,4,5,6,7,8]
 gestures['release'] = [1,2,3,4,5,6,7,8]
 
-f = Figure(figsize=(5, 5), dpi=100)
-a = f.add_subplot(421)
-b = f.add_subplot(422)
-c = f.add_subplot(423)
-d = f.add_subplot(424)
-e = f.add_subplot(425)
-ff = f.add_subplot(426)
-g = f.add_subplot(427)
-h = f.add_subplot(428)
+class demo_thread(threading.Thread):
+    def __init__(self,e1,e2,e3,e4,e5,e6,e7,e8,ax1,ax2,ax3,ax4,ax5,ax6,ax7,ax8,canvas):
+        threading.Thread.__init__(self)
+        self.e1 = e1
+        self.e2 = e2
+        self.e3 = e3
+        self.e4 = e4
+        self.e5 = e5
+        self.e6 = e6
+        self.e7 = e7
+        self.e8 = e8
+        self.ax1=ax1
+        self.ax2=ax2
+        self.ax3=ax3
+        self.ax4=ax4
+        self.ax5=ax5
+        self.ax6=ax6
+        self.ax7=ax7
+        self.ax8=ax8
+        self.id = id
+        self.canvas = canvas
+    def run(self):
+        #print('hey')
+        flag = True
+        m=100
+        n=100
+        i=2
+        x1 = collections.deque(100*[0],100)
+        x2 = collections.deque(100*[0],100)
+        x3 = collections.deque(100*[0],100)
+        x4 = collections.deque(100*[0],100)
+        x5 = collections.deque(100*[0],100)
+        x6 = collections.deque(100*[0],100)
+        x7 = collections.deque(100*[0],100)
+        x8 = collections.deque(100*[0],100)
+        while flag:
+            
+            random_number1 = random.randint(-3,3)
+            random_number2 = random.randint(-3,3)
+            random_number3 = random.randint(-3,3)
+            random_number4 = random.randint(-3,3)
+            random_number5 = random.randint(-3,3)
+            random_number6 = random.randint(-3,3)
+            random_number7 = random.randint(-3,3)
+            random_number8 = random.randint(-3,3)
+            x1.appendleft(random_number1)
+            x2.appendleft(random_number2)
+            x3.appendleft(random_number3)
+            x4.appendleft(random_number4)
+            x5.appendleft(random_number5)
+            x6.appendleft(random_number6)
+            x7.appendleft(random_number7)
+            x8.appendleft(random_number8)
+            matrix1 = np.random.normal(0,1,m*n).reshape(m,n)
+            matrix2 = np.random.normal(0,1,m*n).reshape(m,n)
+            matrix3 = np.random.normal(0,1,m*n).reshape(m,n)
+            matrix4 = np.random.normal(0,1,m*n).reshape(m,n)
+            matrix5 = np.random.normal(0,1,m*n).reshape(m,n)
+            matrix6 = np.random.normal(0,1,m*n).reshape(m,n)
+            matrix7 = np.random.normal(0,1,m*n).reshape(m,n)
+            matrix8 = np.random.normal(0,1,m*n).reshape(m,n)
 
+            #print(random_number)
+            #time.sleep(1)
+            try:
+                self.ax1.clear()
+                self.ax2.clear()
+                self.ax3.clear()
+                self.ax4.clear()
+                self.ax5.clear()
+                self.ax6.clear()
+                self.ax7.clear()
+                self.ax8.clear()
+                self.ax1.axis([0, 100, -4, 4])
+                self.ax2.axis([0, 100, -4, 4])
+                self.ax3.axis([0, 100, -4, 4])
+                self.ax4.axis([0, 100, -4, 4])
+                self.ax5.axis([0, 100, -4, 4])
+                self.ax6.axis([0, 100, -4, 4])
+                self.ax7.axis([0, 100, -4, 4])
+                self.ax8.axis([0, 100, -4, 4])
+                self.ax1.plot(np.array(x1))
+                self.ax2.plot(np.array(x2))
+                self.ax3.plot(np.array(x3))
+                self.ax4.plot(np.array(x4))
+                self.ax5.plot(np.array(x5))
+                self.ax6.plot(np.array(x6))
+                self.ax7.plot(np.array(x7))
+                self.ax8.plot(np.array(x8))
+                self.canvas.draw()
+                self.e1.delete(0,tk.END)
+                self.e1.insert(0,str(random_number1))
+                self.e2.delete(0,tk.END)
+                self.e2.insert(0,str(random_number2))
+                self.e3.delete(0,tk.END)
+                self.e3.insert(0,str(random_number3))
+                self.e4.delete(0,tk.END)
+                self.e4.insert(0,str(random_number4))
+                self.e5.delete(0,tk.END)
+                self.e5.insert(0,str(random_number5))
+                self.e6.delete(0,tk.END)
+                self.e6.insert(0,str(random_number6))
+                self.e7.delete(0,tk.END)
+                self.e7.insert(0,str(random_number7))
+                self.e8.delete(0,tk.END)
+                self.e8.insert(0,str(random_number8))
+                #time.sleep(1)
+            except Exception :
+                #print(Exception)
+                flag = False
 
-def animate(i):
-    pullData = open("readings", "r").read()
-    dataList = pullData.split('\n')
-    xList=[]
-    yList=[]
-
-    for eachLine in dataList:
-        if len(eachLine) > 1:
-            x, y = eachLine.split(',')
-            xList.append(int(x))
-            yList.append(int(y))
-
-    global readings
-    readings = yList
-
-    a.clear()
-    b.clear()
-    c.clear()
-    d.clear()
-    e.clear()
-    ff.clear()
-    g.clear()
-    h.clear()
-    a.plot(xList,yList)
-    b.plot(xList,yList)
-    c.plot(xList,yList)
-    d.plot(xList,yList)
-    e.plot(xList,yList)
-    ff.plot(xList,yList)
-    g.plot(xList,yList)
-    h.plot(xList,yList)
-    
-
-
+class save_thread(threading.Thread):
+    def __init__(self,e1,e2,e3,e4,e5,e6,e7,e8):
+        threading.Thread.__init__(self)
+        self.e1 = e1
+        self.e2 = e2
+        self.e3 = e3
+        self.e4 = e4
+        self.e5 = e5
+        self.e6 = e6
+        self.e7 = e7
+        self.e8 = e8
+        self.id = id
+    def run(self):
+        #print('hey')
+        flag = True
+        while flag:
+            random_number1 = random.randint(0,100)
+            random_number2 = random.randint(0,100)
+            random_number3 = random.randint(0,100)
+            random_number4 = random.randint(0,100)
+            random_number5 = random.randint(0,100)
+            random_number6 = random.randint(0,100)
+            random_number7 = random.randint(0,100)
+            random_number8 = random.randint(0,100)
+            #print(random_number)
+            #time.sleep(1)
+            try:
+                self.e1.delete(0,tk.END)
+                self.e1.insert(0,str(random_number1))
+                self.e2.delete(0,tk.END)
+                self.e2.insert(0,str(random_number2))
+                self.e3.delete(0,tk.END)
+                self.e3.insert(0,str(random_number3))
+                self.e4.delete(0,tk.END)
+                self.e4.insert(0,str(random_number4))
+                self.e5.delete(0,tk.END)
+                self.e5.insert(0,str(random_number5))
+                self.e6.delete(0,tk.END)
+                self.e6.insert(0,str(random_number6))
+                self.e7.delete(0,tk.END)
+                self.e7.insert(0,str(random_number7))
+                self.e8.delete(0,tk.END)
+                self.e8.insert(0,str(random_number8))
+                time.sleep(1)
+            except:
+                flag = False
 class MyoArm(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -143,8 +259,19 @@ class DemoPage(tk.Frame):
         LeftFrame.configure(background='black')
         LeftFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1, anchor=tk.N)
 
+        f = Figure(figsize=(5, 5), dpi=100)
+        ax1 = f.add_subplot(421)
+        ax2 = f.add_subplot(422)
+        ax3 = f.add_subplot(423)
+        ax4 = f.add_subplot(424)
+        ax5 = f.add_subplot(425)
+        ax6 = f.add_subplot(426)
+        ax7 = f.add_subplot(427)
+        ax8 = f.add_subplot(428)
+
+        #ax1 = fig.add_pyplot()
         canvas = FigureCanvasTkAgg(f, LeftFrame)
-        canvas.show()
+        canvas.draw()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 
@@ -158,14 +285,14 @@ class DemoPage(tk.Frame):
         v = StringVar(RightFrame, value='No Connection')
 
 
-        entry_1 = tk.Entry(RightFrame, width=10,textvariable=v,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
-        entry_2 = tk.Entry(RightFrame, width=10,textvariable=v,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
-        entry_3 = tk.Entry(RightFrame, width=10,textvariable=v,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
-        entry_4 = tk.Entry(RightFrame, width=10,textvariable=v,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
-        entry_5 = tk.Entry(RightFrame, width=10,textvariable=v,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
-        entry_6 = tk.Entry(RightFrame, width=10,textvariable=v,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
-        entry_7 = tk.Entry(RightFrame, width=10,textvariable=v,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
-        entry_8 = tk.Entry(RightFrame, width=10,textvariable=v,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
+        entry_1 = tk.Entry(RightFrame, width=10,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
+        entry_2 = tk.Entry(RightFrame, width=10,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
+        entry_3 = tk.Entry(RightFrame, width=10,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
+        entry_4 = tk.Entry(RightFrame, width=10,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
+        entry_5 = tk.Entry(RightFrame, width=10,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
+        entry_6 = tk.Entry(RightFrame, width=10,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
+        entry_7 = tk.Entry(RightFrame, width=10,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
+        entry_8 = tk.Entry(RightFrame, width=10,background='black',foreground='#42f4e2',relief=FLAT,highlightthickness=0,bd=15)
 
         entry_1.pack()
         entry_2.pack()
@@ -175,6 +302,9 @@ class DemoPage(tk.Frame):
         entry_6.pack()
         entry_7.pack()
         entry_8.pack()
+
+        demo_thread_controller = demo_thread(entry_1,entry_2,entry_3,entry_4,entry_5,entry_6,entry_7,entry_8,ax1,ax2,ax3,ax4,ax5,ax6,ax7,ax8,canvas)
+        demo_thread_controller.start()
 
 
 
@@ -213,6 +343,11 @@ class AddGesturePage(tk.Frame):
         entry_6.pack()
         entry_7.pack()
         entry_8.pack()
+
+        save_thread_controller = save_thread(entry_1,entry_2,entry_3,entry_4,entry_5,entry_6,entry_7,entry_8)
+        save_thread_controller.start()
+
+
 
 
 
